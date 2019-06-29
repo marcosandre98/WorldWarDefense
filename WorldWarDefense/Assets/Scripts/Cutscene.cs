deaa30481f8803a79df51381527c92214aa93199
eduardovilke <36681539+eduardovilke@users.noreply.github.com>
2019-06-29 16:47:50 -0300

    IA inimigos

--- WorldWarDefense/Assets/Scripts/Cutscene.cs
+++ WorldWarDefense/Assets/Scripts/Cutscene.cs
@@ -10,7 +10,7 @@ public class Cutscene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();
+        //sentences = new Queue<string>();
     }
 
 }
