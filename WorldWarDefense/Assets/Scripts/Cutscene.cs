d37dad2a83d77de0f61fd2d4b3fed57c9735933f
guischneider100 <guilhermeschneider@unochapeco.edu.br>
2019-06-29 11:15:28 -0300

    Cutscene

---
+++ WorldWarDefense/Assets/Scripts/Cutscene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+
+    public Queue<string> frases;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+}
